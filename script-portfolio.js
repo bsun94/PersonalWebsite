1458c4bae01c5f589e97b4cf6e49de24cc531ee0
bsun94 <52805678+bsun94@users.noreply.github.com>
2020-09-11 21:45:53 -0400

    finished up the portfolio page, added animations to images and lightbox feature to them all; also fixed up animation of the textbox on the about page

---
+++ script-portfolio.js
@@ -0,0 +1,43 @@
+const images = document.querySelectorAll('.image');
+
+const imagesOptions = {
+    threshold: 0.8
+};
+
+const imagesObserver = new IntersectionObserver (function(
+    entries,
+    imagesObserver
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('appear');
+                imagesObserver.unobserve(entry.target);
+            }
+        });
+    }, 
+    imagesOptions);
+
+const lightbox = document.createElement('div');  //check createElement
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+
+images.forEach(image => {
+    imagesObserver.observe(image);
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active');  //check eventlisteners
+        const img = document.createElement('img');
+        console.log(image.firstChild)
+        img.src = image.firstChild.nextElementSibling.src;  //check eventlisteners
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild);
+        }
+        lightbox.appendChild(img);
+    });
+});
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return;
+    lightbox.classList.remove('active');
+});
