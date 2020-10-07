c84e9829897a8218415ed171705e39a858f80db5
bsun94 <52805678+bsun94@users.noreply.github.com>
2020-10-07 00:01:06 -0400

    added throttling, and improved slideshow effect

--- js/script-index.js
+++ js/script-index.js
@@ -4,21 +4,31 @@ function frameOpacity (frame) {
     let bounds = frame.getBoundingClientRect();
     frame.style.opacity = Math.min(1, Math.max(bounds.bottom / bounds.height, 0));
 
-    if (bounds.y <= -5 && bounds.bottom >= 0) {
+    if (bounds.y <= -5 && bounds.bottom >= 0 && frame.nextElementSibling) {
         frame.style.zIndex = 1;
         frame.nextElementSibling.style.zIndex = 0;
-        frame.nextElementSibling.style.bottom = bounds.height + bounds.y - 2 + 'px';
-    } else if (bounds.bottom <= 5) {
+        frame.nextElementSibling.style.transform = `translateY(${-bounds.height - bounds.y}px)`;
+    } else if (bounds.bottom <= 5 && frame.nextElementSibling) {
         frame.style.zIndex = 0;
         frame.nextElementSibling.style.zIndex = 1;
-    } else if (bounds.y >= -5) {
-        frame.nextElementSibling.style.bottom = '0px'
+    } else if (bounds.y >= -5 && frame.nextElementSibling) {
+        frame.nextElementSibling.style.transform = 'none';
+    };
+};
+
+function throttler (fn, wait) {
+    let time = Date.now();
+    return function () {
+        if ((time + wait - Date.now()) < 0) {
+            fn();
+            time = Date.now();
+        };
     };
 };
 
 window.addEventListener('scroll', e => {
     frames.forEach(frame => {
-        frameOpacity(frame);
+        throttler(frameOpacity(frame), 75);
     });
 });
 
