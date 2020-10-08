bf56f5374fb551dc904c0f1819d162e7c87e70ee
bsun94 <52805678+bsun94@users.noreply.github.com>
2020-10-07 20:45:33 -0400

    Small tweak made to index.html's js file

--- js/script-index.js
+++ js/script-index.js
@@ -17,7 +17,7 @@ function frameOpacity (frame) {
 };
 
 function throttler (fn, wait) {
-    let time = Date.now();
+    var time = Date.now();
     return function () {
         if ((time + wait - Date.now()) < 0) {
             fn();
